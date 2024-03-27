96f1d751c4969040c0261e53e8d93daf322b7d87
Simon Bae <yassineGAFF@gmail.com>
2024-03-27 13:23:50 +0100

    working on the ui ux

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 comp_s = random();
 player_s = "rock";
 playerScore = 0;
+compScore = 0;
 
 function random() {
     let k = Math.floor(Math.random()*3);
@@ -23,22 +24,28 @@ function paleyround(player_s,comp_s){
         if (player_s == "Rock"){
             if (comp_s == "Paper"){
                 return "I win , Paper beats Rock";
+                compScore++;
             } else {
-                return "You win , Rock beats Scissors";
+                return "You win , Rock beats Scissors"; 
+                playerScore++;
             }
         }
         if (player_s == "Paper"){
             if (comp_s == "Rock"){
                 return "You win , Paper beats Rock";
+                playerScore++;
             } else {
                 return "I win , Scissors beats Paper";
+                compScore++;
             }
         }
         if (player_s == "Scissors"){
             if (comp_s == "Rock"){
                 return "I win , Rock beats Scissors";
+                compScore++;
             } else {
                 return "You win , Scissors beats Paper";
+                playerScore++;
             }
         }
     }
